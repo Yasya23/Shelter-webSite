47d262c3d94c1f11a23dbbd45fdcb209e087899c
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-04-03 13:48:10 +0300

    feat: check the window size

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -6,26 +6,32 @@ document.addEventListener('click', (e) => {
     renderSlides(e.target.id);
 });
 
+let prevScreenWidth = window.innerWidth;
 let numCardsToShow = calculateNumCardsToShow();
+
+window.onresize = function () {
+  const screenWidth = window.innerWidth;
+  if (screenWidth !== prevScreenWidth) {
+    numCardsToShow = calculateNumCardsToShow();
+    selectRandomCards(numCardsToShow, animals);
+    prevScreenWidth = screenWidth;
+  }
+};
 
 function calculateNumCardsToShow() {
   const screenWidth = window.innerWidth;
   let num = 0;
-  screenWidth < 601 ? (num = 1) : screenWidth < 601 ? (num = 2) : (num = 3);
+  screenWidth < 601 ? (num = 1) : screenWidth < 1053 ? (num = 2) : (num = 3);
   return num;
 }
 
-window.onresize = function (event) {
-  calculateNumCardsToShow();
-  selectRandomCards(numCardsToShow, animals);
-};
-
-function renderCards() {
+function renderCards(cardsArray) {
+  console.log(cardsArray);
   let cardsList = document.querySelector('.slider__cards');
   let cards = '';
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < cardsArray.length; i++) {
     cards += `<li class="slider__card card">
-                  <img class="card__img" src=${animals[i].img} alt=${animals[i].alt}>
+                  <img class="card__img" src=${cardsArray[i].img} alt=${cardsArray[i].alt}>
                   <p class="card__title">Katrine</p>
                   <button class="card__button button button-outline">Learn more</button>
                 </li>`;
