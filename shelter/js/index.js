70b32b2a022354bcc7ae50c419a75c859e8830de
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-04-07 14:58:37 +0300

    feat: finish modal window js

--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,7 +1,10 @@
+import animals from './data.js';
+
 const card = document.querySelector('.card');
 const modalWindow = document.querySelector('.modal');
 
 document.addEventListener('click', (e) => {
+  console.log();
   const clickedCard = e.target.closest('.card');
   if (clickedCard) {
     const id = clickedCard.getAttribute('id');
@@ -11,14 +14,14 @@ document.addEventListener('click', (e) => {
     showPopupWindow(id);
   }
 
-  if (e.target === modalWindow) {
-    console.log(close);
+  if (e.target === modalWindow || e.target.closest('.modal__close-btn')) {
     closeModalWindow();
   }
 });
 
 function showPopupWindow(id) {
   if (id) {
+    renderModalInformation(id);
     modalWindow.style.display = 'block';
     document.body.style.overflowY = 'hidden';
   }
@@ -27,4 +30,49 @@ function showPopupWindow(id) {
 function closeModalWindow() {
   modalWindow.style.display = 'none';
   document.body.style.overflowY = 'visible';
+}
+
+function renderModalInformation(id) {
+  const animal = animals.filter((animal) => animal.name === id);
+  const {
+    age,
+    img,
+    alt,
+    breed,
+    description,
+    diseases,
+    inoculations,
+    name,
+    parasites,
+    type,
+  } = animal[0];
+  console.log(name);
+  modalWindow.innerHTML = `<div id="modal-window" class="modal__wrapper">
+        <button class="modal__close-btn">x</button>
+        <div class="modal__img-box">
+          <img  class="modal__img" src=${img} alt=${alt}>
+        </div>
+        <div class="modal__content">
+          <h3 class="modal__title">${name}</h3>
+          <h4 class="modal__subtitle">${type} - ${breed}</h4>
+          <p class="modal__text">${description}</p>
+          <ul class="modal__list list-modal">
+            <li class="list-modal__item">
+              <span class="list-modal__title">Age:</span>
+              <span class="list-modal__value">${age}</span>
+            </li>
+            <li class="list-modal__item">
+              <span class="list-modal__title">Inoculations:</span>
+              <span class="list-modal__value">${inoculations}</span>
+            </li>
+            <li class="list-modal__item">
+              <span class="list-modal__title">Diseases:</span>
+              <span class="list-modal__value">${diseases}</span>
+            </li>
+            <li class="list-modal__item">
+              <span class="list-modal__title">Parasites:</span>
+              <span class="list-modal__value">${parasites}</span>
+            </li>
+          </ul>
+        </div>`;
 }
